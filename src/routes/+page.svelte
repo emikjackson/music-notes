0bd5f003ffb0b1acedf5953d3910f9bf9f893176
emikjackson <emij@umich.edu>
2022-10-12 22:55:09 -0400

    feat/add playground items

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,41 @@
 <script>
   import Button from '$lib/button.svelte'
-	let count = 0;
+  import { count, time, elapsed } from '../stores.js'
+
   let paused = false;
 
+  // alternative to $count:
+  // let countValue;
+  // const unsubscribe = count.subscribe(value => countValue = value)
+  // onDestroy(unsubscribe);
+
 	const togglePaused = () => paused = !paused;
+  const decrement = () => count.update(n => n - 1);
+  const increment = () => count.update(n => n + 1);
+  const reset = () => count.set(0);
+
 </script>
 
 <svelte:head>
- <title>Different Title</title>
+ <title>Example Title</title>
 </svelte:head>
 
 <div class="tile">
-  <h1>Different Title</h1>
+  <h1>Example Title</h1>
   <p>Visit <a href="/about">about</a> page</p>
-  <Button onClick={togglePaused}>
+  <Button on:click={togglePaused}>
     {paused ? 'Play' : 'Pause'}
   </Button>
+
+  <h3>Counter</h3>
+  <p>{$count}</p>
+  <button on:click={increment}>+</button>
+  <button on:click={decrement}>-</button>
+  <button on:click={reset}>reset</button>
+
+  <h3>Time</h3>
+  <p>The time is {$time}.</p>
+  <p>This page has been open for {$elapsed} seconds.</p>
 </div>
 
 <style>
