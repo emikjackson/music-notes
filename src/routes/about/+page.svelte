762b94e15853b0ea9ac1c8d6d7c4148ca1a3e3e4
emikjackson <emij@umich.edu>
2022-11-18 21:54:16 -0800

    update about text

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -4,9 +4,9 @@
 
 <div class="tile">
   <h1>About</h1>
-  <p>Site made for my pops, Alan Jackson, to perhaps help train his ear.</p>
+  <p>Site made at the request of my pops, Alan Jackson, to perhaps help train his ear.</p>
   <p>The beautiful tones you hear come straight from Alan's guitar.</p>
-  <p>Created by Emi Jackson in Svelte.</p>
+  <p>Created by Emi in Svelte.</p>
 </div>
 
 
@@ -14,10 +14,14 @@
   h1 {
     text-align: center;
   }
+  p {
+    text-align: center;
+  }
   .tile {
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
+    padding: 20px;
   }
 </style>
